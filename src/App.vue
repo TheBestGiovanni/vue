8f3110a74ab813b53844d1208388a1784696b22c
TheBestGiovanni <54220179+TheBestGiovanni@users.noreply.github.com>
2021-07-08 15:24:02 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="raulo">Giovanni e l'informatica :D</h1>
+
+  <div class="space"></div>
+
+  <div class="fackerbygionny">
+    
+    <h1 class="cool">
+      Ciao questa è una prova bella!
+      Questo testo non ha nessun senso e non ha 
+      nessuna ragione di esistere lol
+    </h1>
+
+  </div>
+</template>
